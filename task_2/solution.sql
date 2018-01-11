3a77a0e39a1f94b0ffad0d6f2531fdf8e8cc1e9b
Ivan Makarov <ivan.s.makarov@yandex.ru>
2018-01-11 13:37:20 +0300

    rewrite task_2

--- task_2/solution.sql
+++ task_2/solution.sql
@@ -1,21 +1,50 @@
-CREATE TABLE Months (
-	Month VARCHAR(255),
-	Days VARCHAR(255)
-);
-
-INSERT INTO Months
-VALUES
-	('Jan', day(last_day(concat(year(curdate()), '-01-01')))),
-	('Feb', day(last_day(concat(year(curdate()), '-02-01')))),
-	('Mar', day(last_day(concat(year(curdate()), '-03-01')))),
-	('Apr', day(last_day(concat(year(curdate()), '-04-01')))),
-	('May', day(last_day(concat(year(curdate()), '-05-01')))),
-	('Jun', day(last_day(concat(year(curdate()), '-06-01')))),
-	('Jul', day(last_day(concat(year(curdate()), '-07-01')))),
-	('Aug', day(last_day(concat(year(curdate()), '-08-01')))),
-	('Sep', day(last_day(concat(year(curdate()), '-09-01')))),
-	('Oct', day(last_day(concat(year(curdate()), '-10-01')))),
-	('Nov', day(last_day(concat(year(curdate()), '-11-01')))),
-	('Dec', day(last_day(concat(year(curdate()), '-12-01'))))
-
-SELECT * FROM Months
+with months as (
+	select 	datename(month, concat(year(getdate()),'-01','-01')) as month,
+			month(concat(year(getdate()),'-01','-01')) as monthN,
+			day(eomonth(concat(year(getdate()),'-01','-01'))) as days
+    union
+    select	datename(month, concat(year(getdate()),'-02','-01')) as month,
+			month(concat(year(getdate()),'-02','-01')) as monthN,
+			day(eomonth(concat(year(getdate()),'-02','-01'))) as days
+    union
+    select	datename(month, concat(year(getdate()),'-03','-01')) as month,
+			month(concat(year(getdate()),'-03','-01')) as monthN,
+			day(eomonth(concat(year(getdate()),'-03','-01'))) as days
+    union
+    select	datename(month, concat(year(getdate()),'-04','-01')) as month,
+			month(concat(year(getdate()),'-04','-01')) as monthN,
+			day(eomonth(concat(year(getdate()),'-04','-01'))) as days
+    union
+    select	datename(month, concat(year(getdate()),'-05','-01')) as month,
+			month(concat(year(getdate()),'-05','-01')) as monthN,
+			day(eomonth(concat(year(getdate()),'-05','-01'))) as days
+    union
+    select	datename(month, concat(year(getdate()),'-06','-01')) as month,
+			month(concat(year(getdate()),'-06','-01')) as monthN,
+			day(eomonth(concat(year(getdate()),'-06','-01'))) as days
+    union
+    select	datename(month, concat(year(getdate()),'-07','-01')) as month,
+			month(concat(year(getdate()),'-07','-01')) as monthN,	
+			day(eomonth(concat(year(getdate()),'-07','-01'))) as days
+    union
+    select	datename(month, concat(year(getdate()),'-08','-01')) as month,
+			month(concat(year(getdate()),'-08','-01')) as monthN,
+			day(eomonth(concat(year(getdate()),'-08','-01'))) as days
+    union
+    select	datename(month, concat(year(getdate()),'-09','-01')) as month,
+			month(concat(year(getdate()),'-09','-01')) as monthN,
+			day(eomonth(concat(year(getdate()),'-09','-01'))) as days
+    union
+    select	datename(month, concat(year(getdate()),'-10','-01')) as month,
+			month(concat(year(getdate()),'-10','-01')) as monthN,			
+			day(eomonth(concat(year(getdate()),'-10','-01'))) as days
+    union
+    select	datename(month, concat(year(getdate()),'-11','-01')) as month,
+			month(concat(year(getdate()),'-11','-01')) as monthN,
+			day(eomonth(concat(year(getdate()),'-11','-01'))) as days
+    union
+    select 	datename(month, concat(year(getdate()),'-12','-01')) as month,
+			month(concat(year(getdate()),'-12','-01')) as monthN,
+			day(eomonth(concat(year(getdate()),'-12','-01'))) as days
+) 
+select month, days from months order by monthN;
